4478dd2a98e92e30af192085c08e8e04e75323aa
DESKTOP-MQ <skylife927@gmail.com>
2022-10-27 22:41:33 +0900

    first commit

---
+++ app/src/main/java/com/mungyu/receivercall/PhoneReceiver.kt
@@ -0,0 +1,12 @@
+package com.mungyu.receivercall
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class PhoneReceiver : BroadcastReceiver() {
+
+    override fun onReceive(context: Context?, intent: Intent?) {
+
+    }
+}
